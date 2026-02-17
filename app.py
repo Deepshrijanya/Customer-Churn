fd2f236c76dc0efd3d3f92657b9e24e32bbcb78b
Deepshrijanya <deepshrijanya1140.be22@chitkarauniversity.edu.in>
2026-02-18 01:30:56 +0530

    feat: Implement initial customer churn prediction system with data, model, and application.

---
+++ app.py
@@ -0,0 +1,8 @@
+# streamlit app will be here
+# Gender -> 1 if Female, 0 if Male
+# Churn -> 1 if Yes, 0 if No
+# Scaler is exported as scaler.pkl
+# Model is exported as model.pkl
+# Order of the X is 'Age', 'Gender', 'Tenure', 'MonthlyCharges'
+
+import 
